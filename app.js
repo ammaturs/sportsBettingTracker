12d6ed9cac36e3468aef3d698a0bed8c7d26f6fd
ammaturs <140657677+ammaturs@users.noreply.github.com>
2023-10-25 17:34:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-//TODO
-//session storage so if a user refreshes the page their stuffs still there
-//constantly checking for api updates i.e when a user scores etc
 let session = [];
 //sessionStorage.clear()
 
 document.addEventListener('DOMContentLoaded', function() {
     
     //check if theres a session
-    console.log('loaded12')
-    if (sessionStorage.length>0){
-        let key = sessionStorage.key(0);
+    
+    if (sessionStorage.length>1){
+        let key = sessionStorage.key(1);
         let valueString = sessionStorage.getItem(key);
         let value = JSON.parse(valueString);
-        console.log(`here: ${value}`)
-        for (let i =0; i<value.length; i++)
+        for (let i = 0; i<value.length; i++)
         {
-            console.log(`heyy`);
+            console.log(`${i+1}) ${value[i].player}`);
             loadSession(value[i]);
+           
         }
     }
     
@@ -150,6 +147,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         let gameID;
         let entry;
+        
 
         fetch(`https://statsapi.web.nhl.com/api/v1/schedule?teamId=${teamID}&startDate=${todaysDate}&endDate=${todaysDate}`)
         .then(response => response.json())
@@ -167,15 +165,54 @@ document.addEventListener('DOMContentLoaded', function() {
     //using the playerid and game id from previous functions, this function gets the player data as specified by user i.e points, goals, shots etc
     function get_player_data(playerID, gameID)
     {
+        let human = document.querySelector('#player').value;
         const menu = document.querySelector('#menu').value;
         let update;
+        let period_interval, period_time;
 
         fetch(`https://statsapi.web.nhl.com/api/v1/game/${gameID}/feed/live`)
         .then(response => response.json())
         .then(data => {
             const liveData = data.liveData;
-            const boxscore = liveData.boxscore;
+            let boxscore;
+            try{
+                boxscore = liveData.boxscore;
+            }
+            catch(err) 
+            {
+                alert(`Oops, ${human} doesn't play today.`)
+                return;
+            }
             const teams = boxscore.teams;
+            //get period variables
+            plays = liveData.plays
+            all_plays = plays.allPlays
+
+            //we only care about the most recent time/period here, therefore take last entry in all_plays array
+            //if game didnt start yet, these entries dont exist yet
+            try
+            {
+                period_interval = all_plays[all_plays.length-1].about.period
+                period_time = all_plays[all_plays.length-1].about.periodTimeRemaining
+            }
+            catch(err)
+            {
+                //lets get the time of the game
+                let game_data = data.gameData.datetime.dateTime;
+                let testing = game_data.split('T')[1];
+                let working2 = testing.split(':')
+                let actual_time = working2[0] + ':' + working2[1]
+         
+
+                // Example usage
+                const inputTime = actual_time;
+                const originalTimeZone = 'UTC';
+                const targetTimeZone = 'America/New_York';
+
+                convertedTime = convertTimeToTimeZone(inputTime, originalTimeZone, targetTimeZone);
+            
+                
+            }
             // Loop through the away and home teams
        
             for (const entry in teams) 
@@ -202,7 +239,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }//for loop close
             
             
-            loadPlayer(update, playerID);
+            loadPlayer(update, playerID, period_interval, period_time, convertedTime);
 
         })//then data close
     }// get_player_data close
@@ -210,9 +247,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
- function loadSession(obj)
+function loadSession(obj)
  {
-    
+
     let human = obj.player;
     let menu = obj.menu;
     let wager = obj.wager;
@@ -220,6 +257,8 @@ document.addEventListener('DOMContentLoaded', function() {
     let playerID, teamID;
     let gameID, entry;
     let update;
+    let period_interval, period_time, convertedTime;
+    
 
     let userData = {
         player: human, //player
@@ -273,12 +312,43 @@ document.addEventListener('DOMContentLoaded', function() {
             fetch(`https://statsapi.web.nhl.com/api/v1/game/${gameID}/feed/live`)
             .then(response => response.json())
             .then(data3 => {
+                //get stat variables
                 const liveData = data3.liveData;
                 const boxscore = liveData.boxscore;
                 const teams = boxscore.teams;
+
+                //get period variables
+                plays = liveData.plays
+                all_plays = plays.allPlays
+    
+                //we only care about the most recent time/period here, therefore take last entry in all_plays array
+                //if game didnt start yet, these entries dont exist yet
+                try
+                {
+                    period_interval = all_plays[all_plays.length-1].about.period
+                    period_time = all_plays[all_plays.length-1].about.periodTimeRemaining
+                }
+                catch(err)
+                {
+                    //lets get the time of the game
+                    let game_data = data3.gameData.datetime.dateTime;
+                    let testing = game_data.split('T')[1];
+                    let working2 = testing.split(':')
+                    let actual_time = working2[0] + ':' + working2[1]
+                    
+
+                    // Example usage
+                    const inputTime = actual_time;
+                    const originalTimeZone = 'UTC';
+                    const targetTimeZone = 'America/New_York';
+
+                    convertedTime = convertTimeToTimeZone(inputTime, originalTimeZone, targetTimeZone);
+                
+                    
+                }
+
                 // Loop through the away and home teams
-        
-                for (const entry in teams) 
+                for (const entry in teams) //this loop gets the stat we wanna track i.e shots, assists etc
                 {
                     const oneSide = teams[entry];
                 
@@ -304,52 +374,79 @@ document.addEventListener('DOMContentLoaded', function() {
                 // Create a new div element, this will be the div where our bets are shown
                 const total = document.createElement('div');
                 const entry2 = document.createElement('div'); //the main bet div
+                const info = document.createElement('div'); // div to align data and period
                 const data = document.createElement('div'); //the bet details div
+                const period = document.createElement('div'); //period details div
                 const progress = document.createElement('div'); //the bet progress div
                 const x = document.createElement('button');
 
+                //total is the main
+                //entry is a child of total
+                //info is a child of entry, image is a child of entry, progress is a child of entry, x button is a child of entry
+                //data and period are children of info
+
                 total.appendChild(entry2);
-                total.style.display = 'block';
+                total.classList.add('entire_entry')
 
                 //add border to each entry & format
                 entry2.classList.add('bet_border');
-                entry2.style.display = 'flex'; 
+
 
                 //player image
                 const imgElement = document.createElement('img');
-                imgElement.src = `http://nhl.bamcontent.com/images/headshots/current/168x168/${playerID}.jpg`
+                imgElement.src = `images/${playerID}.jpg`;
                 imgElement.classList.add('image_style');
                 entry2.appendChild(imgElement);
-                console.log('passed')
 
                 // Set the inner HTML of the bet data div which is a child of the entry div 
-                data.innerHTML = `<span id=bet_entry> <strong> ${human} &nbsp &nbsp &nbsp &nbsp</strong> <strong> &nbsp</strong> ${wager}${val} &nbsp<span style= text-transform:capitalize;>${menu}</span> </span>`;
-            
-                //format bet details
-                entry2.appendChild(data);
-                //data.style.display = 'block';
+                data.innerHTML = `<strong style=font-size:large;> ${human} &nbsp</strong> <strong> &nbsp</strong><span style=font-size:large;> ${wager}${val} &nbsp<span style= text-transform:capitalize;>${menu}</span></span>`;
+                data.classList.add('bet_entry');
                 
+                //SET PERIOD VALS HERE
+                
+                //end of game
+                if (parseInt(period_interval) == 3 && period_time == '00:00')
+                {
+                    period.innerHTML = `<strong>Game Over</strong>`
+                }
+                //game didnt start yet
+                else if (period_interval == undefined)
+                {
+                    period.innerHTML = `<strong>Game Starts at ${convertedTime} </strong>`
+                }
+                else
+                {
+                    period.innerHTML = `<br>Period: <strong>${period_interval}</strong> <br> Time Remaining: <strong>${period_time}</strong>`
+                }
+                
+                period.classList.add('period_entry')
+
+                info.appendChild(data)
+                info.appendChild(period)
+                entry2.appendChild(info);
+                info.classList.add('info_entry');
+
+         
                 //set the inner html of the progress of the bet div which is a child of the entry div
                 if (update === undefined)
                 {
-                    progress.innerHTML= `<span id = progress>0</span>`;
+                    progress.innerHTML= 0;
                 }
                 else{
-                    progress.innerHTML= `<span id = progress>${update}</span>`;
+                    progress.innerHTML= update;
                 }
-
-                progress.style.textAlign = 'right';
+                progress.classList.add('bet_progress')
+                
                 
                 entry2.appendChild(progress);
 
                 //button
-                x.innerHTML = `<button id="close_bet" type="button" class="btn-close" aria-label="Close"></button>`;
-                x.style.marginRight = '10px'; 
-                x.style.textAlign = 'right';
+                x.innerHTML = `<button type="button" class="btn-close" aria-label="Close"></button>`;
+                x.classList.add('close_bet')
                 entry2.appendChild(x);
                 
 
-                let key = sessionStorage.key(0);
+                let key = sessionStorage.key(1);
                 let valueString = sessionStorage.getItem(key);
                 let value = JSON.parse(valueString);
 
@@ -388,10 +485,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 //new bet, renders html to display new bet
-  function loadPlayer(update, playerID)
+  function loadPlayer(update, playerID, period_interval, period_time, convertedTime)
   {
-    console.log('loadPlayer');
-
     const human = document.querySelector('#player').value; //specified player
     const menu = document.querySelector('#menu').value; //shots? goals? assists?
     const wager = document.querySelector('#wager').value; //over or under?
@@ -406,51 +501,77 @@ document.addEventListener('DOMContentLoaded', function() {
     };
     
     
-    // Create a new div element, this will be the div where our bets are shown
-    const total = document.createElement('div');
-    const entry = document.createElement('div'); //the main bet div
-    const data = document.createElement('div'); //the bet details div
-    const progress = document.createElement('div'); //the bet progress div
-    const x = document.createElement('button');
+     // Create a new div element, this will be the div where our bets are shown
+     const total = document.createElement('div');
+     const entry = document.createElement('div'); //the main bet div
+     const info = document.createElement('div'); // div to align data and period
+     const data = document.createElement('div'); //the bet details div
+     const period = document.createElement('div'); //period details div
+     const progress = document.createElement('div'); //the bet progress div
+     const x = document.createElement('button');
 
-    total.appendChild(entry);
-    total.style.display = 'block';
+     //total is the main
+     //entry is a child of total
+     //info is a child of entry, image is a child of entry, progress is a child of entry, x button is a child of entry
+     //data and period are children of info
 
-    //add border to each entry & format
-    entry.classList.add('bet_border');
-    entry.style.display = 'flex'; 
+     total.appendChild(entry);
+     total.classList.add('entire_entry')
+
+     //add border to each entry & format
+     entry.classList.add('bet_border');
+
 
     //player image
     const imgElement = document.createElement('img');
-    imgElement.src = `http://nhl.bamcontent.com/images/headshots/current/168x168/${playerID}.jpg`
+    imgElement.src = `images/${playerID}.jpg`;
     imgElement.classList.add('image_style');
     entry.appendChild(imgElement);
-    console.log('passed')
 
     // Set the inner HTML of the bet data div which is a child of the entry div 
-    data.innerHTML = `<span id=bet_entry> <strong> ${human} &nbsp &nbsp &nbsp &nbsp</strong> <strong> &nbsp</strong> ${wager}${val} &nbsp<span style= text-transform:capitalize;>${menu}</span> </span>`;
-   
-    //format bet details
-    entry.appendChild(data);
-    //data.style.display = 'block';
+    data.innerHTML = `<strong style=font-size:large;> ${human} &nbsp</strong> <strong> &nbsp</strong><span style=font-size:large;> ${wager}${val} &nbsp<span style= text-transform:capitalize;>${menu}</span></span>`;
+    data.classList.add('bet_entry');
+
+    //NEEED TO FIX HERE, NEED TO ADD TO THIS FUNCTION WHAT PERIOD_INTERVAL IS, ETC BY PASSING DOWN FROM OTHER FUNCTIONS PROBABLY
+                
+    //end of game
+    if (parseInt(period_interval) == 3 && period_time == '00:00')
+    {
+        period.innerHTML = `<strong>Game Over</strong>`
+    }
+    //game didnt start yet
+    else if (period_interval == undefined)
+    {
+        period.innerHTML = `<strong>Game Starts at ${convertedTime} </strong>`
+    }
+    else
+    {
+        period.innerHTML = `<br>Period: <strong>${period_interval}</strong> <br> Time Remaining: <strong>${period_time}</strong>`
+    }
+    
+    period.classList.add('period_entry')
+
+    info.appendChild(data)
+    info.appendChild(period)
+    entry.appendChild(info);
+    info.classList.add('info_entry');
     
     //set the inner html of the progress of the bet div which is a child of the entry div
-   if (update === undefined)
-   {
-    progress.innerHTML= `<span id = progress>0</span>`;
-   }
-   else{
-    progress.innerHTML= `<span id = progress>${update}</span>`;
-   }
-
-   progress.style.textAlign = 'right';
-   
-   entry.appendChild(progress);
+    if (update === undefined)
+    {
+        progress.innerHTML= 0;
+    }
+    else{
+        progress.innerHTML= update;
+    }
+    progress.classList.add('bet_progress')
+    
+    
+    entry.appendChild(progress);
 
    //button
-   x.innerHTML = `<button id="close_bet" type="button" class="btn-close" aria-label="Close"></button>`;
-   x.style.marginRight = '10px'; 
-   x.style.textAlign = 'right';
+   x.innerHTML = `<button type="button" class="btn-close" aria-label="Close"></button>`;
+   x.classList.add('close_bet')
    entry.appendChild(x);
 
    //this function deals with new bets, therefore everytime its called add to session    
@@ -459,9 +580,10 @@ document.addEventListener('DOMContentLoaded', function() {
    // Convert the object to a JSON string and store it in sessionStorage, stores the entire array in sessionStorage
    sessionStorage.setItem('sesh', JSON.stringify(session));
 
-   let key = sessionStorage.key(0);
+   let key = sessionStorage.key(1);
    let valueString = sessionStorage.getItem(key);
    let value = JSON.parse(valueString);
+
    console.log(`here: ${valueString}`)
    
    x.addEventListener('click', function() {
@@ -488,11 +610,31 @@ document.addEventListener('DOMContentLoaded', function() {
     
     
     document.querySelector('#player').value='';
-    document.querySelector('#menu').value = "What are we betting on?"; // Reset menu to placeholder option
-    document.querySelector('#wager').value = 'Prop'; // Reset wager to placeholder option
-    document.querySelector('#stat').value='';
+    //document.querySelector('#menu').value = "What are we betting on?"; // Reset menu to placeholder option
+    //document.querySelector('#wager').value = 'Prop'; // Reset wager to placeholder option
+    //document.querySelector('#stat').value='';
 
   }//function close
+
+
+
+  //function which helps us get the time of the game
+  function convertTimeToTimeZone(timeString, originalTimeZone, targetTimeZone) 
+  {
+    // Parse the input time string
+    const [hours, minutes] = timeString.split(':').map(Number);
+
+    // Create a Date object with the original time and UTC time zone
+    const originalDate = new Date(Date.UTC(2023, 9, 25, hours, minutes)); // Month is 0-based (9 represents October)
+
+    // Create a new Date object with the target time and the target time zone
+    const targetDate = new Date(originalDate.toLocaleString('en-US', { timeZone: targetTimeZone }));
+
+    // Format the target time as a string
+    const targetTimeString = targetDate.toLocaleTimeString([], { timeZoneName: 'short', hour: '2-digit', minute: '2-digit' });
+
+    return targetTimeString;
+}
 
  
  
